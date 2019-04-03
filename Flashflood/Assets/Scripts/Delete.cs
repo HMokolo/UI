193293c0c24a3248f45c5ba019b6f8c2279470f9
Hilary Mokolo <HMokolo@users.noreply.github.com>
2019-04-03 07:11:34 -0500

    Flood Watch Unity
    The unity console. We have to merge into Capston architecture Repo

---
+++ Flashflood/Assets/Scripts/Delete.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Delete : MonoBehaviour {
+
+    [SerializeField]
+    GameObject objectToDestroy;
+    public void DestroyGameObject()
+    {
+        //Destroy Game object
+        Destroy(objectToDestroy);
+
+        Debug.Log("I am deleting a model!");
+        }
+}
